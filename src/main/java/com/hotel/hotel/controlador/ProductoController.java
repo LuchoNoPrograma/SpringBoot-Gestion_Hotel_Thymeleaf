58a4c70d0fdb54a40d5a922c59dc34ae039b7225
Haki <santiago.lahor.roca@gmail.com>
2023-07-09 20:59:42 -0400

    si

---
+++ src/main/java/com/hotel/hotel/controlador/ProductoController.java
@@ -0,0 +1,86 @@
+package com.hotel.hotel.controlador;
+
+import com.hotel.hotel.modelo.entidad.Empleado;
+import com.hotel.hotel.modelo.entidad.Persona;
+import com.hotel.hotel.modelo.entidad.Producto;
+import com.hotel.hotel.modelo.enums.Cargo;
+import com.hotel.hotel.modelo.enums.Estado;
+import com.hotel.hotel.modelo.enums.EstadoCivil;
+import com.hotel.hotel.modelo.enums.Sexo;
+import com.hotel.hotel.modelo.servicio.interfaces.IEmpleadoService;
+import com.hotel.hotel.modelo.servicio.interfaces.IPersonaService;
+import com.hotel.hotel.modelo.servicio.interfaces.IProductoService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/producto")
+public class ProductoController {
+  private final IProductoService productoService;
+  
+
+  @GetMapping("/lista")
+  public String inicio(Model model) {
+    model.addAttribute("template", "layout");
+    model.addAttribute("title", "Lista de empleados");
+    model.addAttribute("listaProductos", productoService.findAll(Sort.by("idProducto")));
+    model.addAttribute("fragmento", "tabla");
+    return "app/producto";
+  }
+
+  @GetMapping("/formulario-registro")
+  public String formularioRegistro(Model model){
+    Producto producto = new Producto();
+    model.addAttribute("template", "layout");
+    model.addAttribute("title", "Lista de productos");
+    model.addAttribute("fragmento", "formulario");
+    model.addAttribute("tipoFormulario", "registrar");
+    model.addAttribute("producto", producto);
+    return "app/producto";
+  }
+
+  @GetMapping("/formulario-modificar/{idProducto}")
+  public String formularioModificar(Model model, @PathVariable Long idProducto){
+    Producto producto = productoService.findById(idProducto);
+
+    model.addAttribute("template", "layout");
+    model.addAttribute("title", "Modificar producto existente");
+    model.addAttribute("fragmento", "formulario");
+    model.addAttribute("tipoFormulario", "modificar");
+    model.addAttribute("producto", producto);
+    return "app/producto";
+  }
+
+  @PostMapping("/registrar")
+  public String registrar(@ModelAttribute Producto producto, RedirectAttributes flash){
+    producto.setEstado(Estado.ACTIVO);
+    productoService.save(producto);
+    flash.addFlashAttribute("exito", "Producto registrado exitosamente");
+    return "redirect:/producto/lista";
+  }
+
+  @PostMapping("/modificar")
+  public String modificar(@ModelAttribute Producto producto, RedirectAttributes flash){
+    productoService.save(producto);
+    flash.addFlashAttribute("exito", "Producto modificado exitosamente");
+    return "redirect:/producto/lista";
+  }
+
+
+  @PostMapping("/eliminar")
+  public String eliminar(@RequestParam Long idProducto, RedirectAttributes flash){
+    Producto producto = productoService.findById(idProducto);
+    producto.setEstado(Estado.ELIMINADO);
+    productoService.save(producto);
+    flash.addFlashAttribute("exito", "Producto modificado exitosamente");
+    return "redirect:/producto/lista";
+  }
+}
